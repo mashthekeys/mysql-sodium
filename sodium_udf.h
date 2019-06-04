91c86907f17d1d15ae117295c9fb907f05ae88a5
Andy Holland <github@ahweb.co.uk>
2019-06-04 13:47:26 +0100

    sodium_udf.h: Tidying up.

--- sodium_udf.h
+++ sodium_udf.h
@@ -8,7 +8,10 @@ namespace Sodium {
   #include <sodium.h>
 }
 
-// Used in the macro fixed_buffer
+
+////////////////////////////////////////////////////////////////////////////////
+// Buffer Declarations and Macros
+
 char *dynamic_buffer(char *preallocated, size_t required, char **store);
 
 void free_buffer(char *string);
@@ -18,10 +21,22 @@ void free_buffer(char *string);
     *length = fixedLength;
 
 
+////////////////////////////////////////////////////////////////////////////////
+// Constant Macros
+
 #define MYSQL_BINARY_STRING 16777216
 
 #define mysql_RESULT_LENGTH 255
 
+#define BOUNDARY ((char)0)
+
+#define SUCCESS 0
+
+#define FAIL -1
+
+
+////////////////////////////////////////////////////////////////////////////////
+// UDF Generator Macros
 
 #define MYSQL_STRING_FUNCTION(UDFName, initFunctionBody, mainFunctionBody, deinitFunctionBody) \
 extern "C" char *UDFName(UDF_INIT *initid, UDF_ARGS *args, char *result, unsigned long *length, char *is_null, char *error)\
@@ -31,6 +46,7 @@ extern "C" bool UDFName##_init(UDF_INIT *initid, UDF_ARGS *args, char *message)\
 extern "C" void UDFName##_deinit(UDF_INIT *initid)\
 deinitFunctionBody ;
 
+
 #define MYSQL_INTEGER_FUNCTION(UDFName, initFunctionBody, mainFunctionBody, deinitFunctionBody) \
 extern "C" long long UDFName(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error)\
 mainFunctionBody; \
@@ -38,6 +54,7 @@ extern "C" bool UDFName##_init(UDF_INIT *initid, UDF_ARGS *args, char *message)\
 { { if (Sodium::sodium_init() < 0) return (strcpy(message, "Sodium initialization error"), 1); } initFunctionBody; return 0; }; \
 extern "C" void UDFName##_deinit(UDF_INIT *initid)\
 deinitFunctionBody ;
+
 
 #define MYSQL_DOUBLE_FUNCTION(UDFName, initFunctionBody, mainFunctionBody, deinitFunctionBody) \
 extern "C" double UDFName(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error)\
@@ -48,72 +65,7 @@ extern "C" void UDFName##_deinit(UDF_INIT *initid)\
 deinitFunctionBody ;
 
 
-#define return_MYSQL_NULL(zero) { *is_null = 1; return zero; }
-
-
-#define SUCCESS 0
-
-#define FAIL -1
-
-
-// Handle sodium internal error
-#define MUST_SUCCEED(expression) {\
-    if ((expression) != SUCCESS) {\
-        *error = 1; \
-        return 0;\
-    }\
-};
-
-#define REQUIRE_ARGS(n) {\
-    if (args->arg_count != n) {\
-        strcpy(message, #n " arguments required");\
-        return 1;\
-    }\
-}
-
-#define REQUIRE_STRING(n, label)    {\
-    if (args->arg_type[n] != STRING_RESULT) {\
-        strcpy(message, #label " must be a string");\
-        return 1;\
-    }\
-}
-
-#define REQUIRE_INTEGER(n, label)   {\
-    if (args->arg_type[n] != INT_RESULT) {\
-        strcpy(message, #label " must be an integer");\
-        return 1;\
-    }\
-}
-
-#define REQUIRE_DOUBLE(n, label)    {\
-    if (args->arg_type[n] != DOUBLE_RESULT) {\
-        strcpy(message, #label " must be a double");\
-        return 1;\
-    }\
-}
-
-#define REQUIRE_CONST_STRING(n, label)    {\
-    if (args->arg_type[n] != STRING_RESULT || args->args[n] == NULL) {\
-        strcpy(message, #label " must be a constant string");\
-        return 1;\
-    }\
-}
-
-#define REQUIRE_CONST_INTEGER(n, label)   {\
-    if (args->arg_type[n] != INT_RESULT || args->args[n] == NULL) {\
-        strcpy(message, #label " must be a constant integer");\
-        return 1;\
-    }\
-}
-
-#define REQUIRE_CONST_DOUBLE(n, label)    {\
-    if (args->arg_type[n] != DOUBLE_RESULT || args->args[n] == NULL) {\
-        strcpy(message, #label " must be a constant double");\
-        return 1;\
-    }\
-}
-
-#define SUBSTRING_FUNCTION(substr_udf, substr_field, substr_max_length, substr_offset, substr_length, total_field_length) \
+#define SUBSTRING_FUNCTION(substr_udf, substr_field, substr_max_length, substr_offset, substr_length, boundary_pos, total_field_length) \
 MYSQL_STRING_FUNCTION(substr_udf, \
 { init: \
     REQUIRE_ARGS(1); \
@@ -122,7 +74,10 @@ MYSQL_STRING_FUNCTION(substr_udf, \
 }, \
 { main: \
     char *const substr_field = args->args[0]; \
-    if (substr_field == NULL || args->lengths[0] != (total_field_length)) { \
+    if (substr_field == NULL \
+        || args->lengths[0] != (total_field_length) \
+        || substr_field[boundary_pos] != BOUNDARY \
+    ) { \
         return_MYSQL_NULL(NULL); \
     } \
     *length = (size_t)(substr_length); \
@@ -131,9 +86,7 @@ MYSQL_STRING_FUNCTION(substr_udf, \
 { deinit: \
     if (initid->ptr != NULL)  free(initid->ptr); \
 });
-//    result = fixed_buffer(result, (substr_length), initid->ptr); \
-//    strcpy(result, substr_field + (substr_offset), (substr_length)); \
-//    return result; \
+
 
 #define BUFFER_GENERATOR_FUNCTION(buffer_udf, generator_function, buffer_length, _max_length) \
 MYSQL_STRING_FUNCTION(buffer_udf, \
@@ -148,6 +101,7 @@ MYSQL_STRING_FUNCTION(buffer_udf, \
     if (initid->ptr != NULL)  free(initid->ptr); \
 });
 
+
 #define UDF_STRING_ALIAS(alias_udf, implementation_udf) \
 extern "C" char* alias_udf(UDF_INIT *initid, UDF_ARGS *args, char *result, unsigned long *length, char *is_null, char *error)\
 { return implementation_udf(initid, args, result, length, is_null, error); } \
@@ -155,6 +109,7 @@ extern "C" bool alias_udf##_init(UDF_INIT *initid, UDF_ARGS *args, char *message
 { return implementation_udf##_init(initid, args, message); } \
 extern "C" void alias_udf##_deinit(UDF_INIT *initid)\
 { return implementation_udf##_deinit(initid); }
+
 
 #define UDF_INTEGER_ALIAS(alias_udf, implementation_udf) \
 extern "C" long long alias_udf(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error)\
@@ -164,12 +119,85 @@ extern "C" bool alias_udf##_init(UDF_INIT *initid, UDF_ARGS *args, char *message
 extern "C" void alias_udf##_deinit(UDF_INIT *initid)\
 { return implementation_udf##_deinit(initid); }
 
+
 #define UDF_DOUBLE_ALIAS(alias_udf, implementation_udf) \
 extern "C" double alias_udf(UDF_INIT *initid, length, is_null, error); } \
 extern "C" bool alias_udf##_init(UDF_INIT *initid, UDF_ARGS *args, char *message)\
 { return implementation_udf##_init(initid, args, message); } \
 extern "C" void alias_udf##_deinit(UDF_INIT *initid)\
 { return implementation_udf##_deinit(initid); }
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Utiility Macros
+
+
+#define return_MYSQL_NULL(zero) { *is_null = 1; return zero; }
+
+
+#define MUST_SUCCEED(expression) {\
+    if ((expression) != SUCCESS) {\
+        *error = 1; \
+        return 0;\
+    }\
+};
+
+
+#define REQUIRE_ARGS(n) {\
+    if (args->arg_count != n) {\
+        strcpy(message, #n " arguments required");\
+        return 1;\
+    }\
+}
+
+
+#define REQUIRE_STRING(n, label)    {\
+    if (args->arg_type[n] != STRING_RESULT) {\
+        strcpy(message, #label " must be a string");\
+        return 1;\
+    }\
+}
+
+
+#define REQUIRE_INTEGER(n, label)   {\
+    if (args->arg_type[n] != INT_RESULT) {\
+        strcpy(message, #label " must be an integer");\
+        return 1;\
+    }\
+}
+
+
+#define REQUIRE_DOUBLE(n, label)    {\
+    if (args->arg_type[n] != DOUBLE_RESULT) {\
+        strcpy(message, #label " must be a double");\
+        return 1;\
+    }\
+}
+
+
+#define REQUIRE_CONST_STRING(n, label)    {\
+    if (args->arg_type[n] != STRING_RESULT || args->args[n] == NULL) {\
+        strcpy(message, #label " must be a constant string");\
+        return 1;\
+    }\
+}
+
+
+#define REQUIRE_CONST_INTEGER(n, label)   {\
+    if (args->arg_type[n] != INT_RESULT || args->args[n] == NULL) {\
+        strcpy(message, #label " must be a constant integer");\
+        return 1;\
+    }\
+}
+
+
+#define REQUIRE_CONST_DOUBLE(n, label)    {\
+    if (args->arg_type[n] != DOUBLE_RESULT || args->args[n] == NULL) {\
+        strcpy(message, #label " must be a constant double");\
+        return 1;\
+    }\
+}
+
 
 #endif
 ;
